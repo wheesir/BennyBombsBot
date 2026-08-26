a963122af638b1d954ae62ec5c40fdd1b5df8fc7
wheesir <jeffwieser19@gmail.com>
2026-08-26 14:18:52 -0500

    Update peg.js
    adding special peg fun for Wednesdays

--- commands/fun/peg.js
+++ commands/fun/peg.js
@@ -1,11 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
-
 module.exports = {
-        data: new SlashCommandBuilder()
-                .setName(`peg`)
-                .setDescription(`Peg?`),
-        execute(interaction) {
-                interaction.reply(`How much Peg could Peg's peg peg if Peg's peg could peg Peg?`);
-
-        },
+	data: new SlashCommandBuilder()
+		.setName(`peg`)
+		.setDescription(`Peg?`),
+	execute(interaction) {
+		const today = new Date().toLocaleString('en-us', { weekday: 'long' });
+		if (today === 'Wednesday') {
+			interaction.reply(`https://klipy.com/gifs/batpeg-batpeg-wednesday`);
+		} else {
+			interaction.reply(`How much Peg could Peg's peg peg if Peg's peg could peg Peg?`);
+		}
+	},
 };
